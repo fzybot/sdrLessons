67a51560abc967a709635cd51b2a0aa671ffd359
fzybot <ruslan.akhpashev@gmail.com>
2024-12-21 16:37:05 +0700

    refactor and update src/main

--- tests/scripts/plot_pcm.py
+++ tests/scripts/plot_pcm.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Открываем файл для чтения
-name = "/home/fzybot/dev/sdrLessons/build/tests/soapy_pluto/txdata.pcm"
+name = "/home/novosibirsk/dev/sdrLessons/build/tests/soapy_pluto/txdata.pcm"
 
 data = []
 imag = []
